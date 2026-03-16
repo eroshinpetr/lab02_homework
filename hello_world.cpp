7bf60df0a6c6a2e42ab21f2385eddffc657ba90f
eroshinpetr <eroshinpetr1@gmail.com>
2026-03-17 01:13:36 +0300

    Add comments to code

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,13 @@
 #include <string>
 
 int main() {
-    std::string name;
+    std::string name;  // переменная для хранения имени пользователя
+    
+    // Запрашиваем имя пользователя
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
+    
+    // Выводим приветствие
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
